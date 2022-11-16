238400091dfa7fa24171b68040e53413fe942f6c
PoSayDone <poseaydone@ya.ru>
2022-11-16 16:41:42 +0500

    Added static check_key method

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 import sys
-import types
 
 from design import Ui_MainWindow 
 from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit
-from PySide6.QtCore import QEvent, Qt
+from PySide6.QtCore import Qt
 
 class LineEdit(QLineEdit):
     def __init__(self):
         super(LineEdit, self).__init__()
         self.final_text = ''
+    
+    @staticmethod
+    def check_key(letter):
+        return (letter.text().isalnum() or 
+                letter.key() == Qt.Key.Key_Colon or 
+                letter.key() == Qt.Key.Key_Semicolon)
 
     def keyPressEvent(self, keyEvent):
         letter = keyEvent
-        if letter.key() not in (Qt.Key.Key_Shift, Qt.Key.Key_Escape, Qt.Key.Key_Backspace):
+        if self.check_key(letter):
             self.replace_letter(letter.text())
-        if letter.key() == (Qt.Key.Key_Backspace) and self.cursorPosition() > 0:
+        elif (letter.key() == Qt.Key.Key_Backspace) and (self.cursorPosition() > 0):
             self.remove_letter()
 
     def replace_letter(self, letter):
@@ -27,10 +32,8 @@ class LineEdit(QLineEdit):
     def remove_letter(self):
         position = self.cursorPosition() -1 
         replaced_text = self.get_entry_text()[:position] + self.final_text[position:]
-        print(replaced_text)
-        if (position != 0):
-            self.set_entry_text(replaced_text)
-            self.setCursorPosition(position)
+        self.set_entry_text(replaced_text)
+        self.setCursorPosition(position)
 
     def get_entry_text(self):
         return (self.text())
