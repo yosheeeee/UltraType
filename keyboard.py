58bee4dd9d38da7c76b875a4c44c2827dfd18290
PoSayDone <poseaydone@ya.ru>
2023-01-18 22:42:22 +0500

    Added comments

--- keyboard.py
+++ keyboard.py
@@ -6,6 +6,7 @@ class Keyboard(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    # Изменяет раскладку на виртуальной клавиатуре
     def set_keyboard_layout(self, keyboard_parent_object, buttons: str):
         button_number = 0
         for btn in config.KEYBOARD_BUTTONS:
@@ -13,8 +14,10 @@ class Keyboard(QWidget):
                 buttons[button_number])
             button_number += 1
 
+    # Меняет цвет у кнопки 
     def highlight_button(self, button):
         button.setStyleSheet("background-color: #68627A")
 
+    # Возвращает первоначальный цвет кнопке 
     def unhighlight_button(self, button):
         button.setStyleSheet("background-color: #302D38")
