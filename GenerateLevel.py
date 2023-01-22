976f69d1637a9327838ebb93b37b1209e848d526
yosheeeee <evantipin@edu.hse.com>
2023-01-22 14:33:58 +0500

    update GenerateLevel.py

--- GenerateLevel.py
+++ GenerateLevel.py
@@ -1,7 +1,14 @@
 import random
 def GenerateLevel(n):
-    letters='fjghdksla;tyrueiwoqp'
-    use_letters=letters[0:n*2]
+    letters='fjghdksla;\'tyrueiwoqp[]vnbcmx,z./'
+    use_letters_len=0
+    if (n>=12):
+        use_letters_len=n*2+2
+    elif(n>=5):
+        use_letters_len=n*2+1
+    else:
+        use_letters_len=n*2
+    use_letters=letters[0:use_letters_len]
     result =[]
     for i in range(20):
         len_word=random.randint(1,6)
@@ -13,6 +20,7 @@ def GenerateLevel(n):
 
 print(GenerateLevel(1))
 print(GenerateLevel(2))
-print(GenerateLevel(3))
+print(GenerateLevel(5))
+print(GenerateLevel(12))
 
 
